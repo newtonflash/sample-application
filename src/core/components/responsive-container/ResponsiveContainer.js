5dc11145a1113e3d86ae8a27d5be32b3c43da7da
newtonflash <newton@newtonflash.com>
2019-06-11 14:09:46 +0530

    Update ResponsiveContainer.js

--- src/core/components/responsive-container/ResponsiveContainer.js
+++ src/core/components/responsive-container/ResponsiveContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ResponsiveContainer = ({ _class = '', children }) => (
-    <div className={`responsive-container ${_class}`}>{children}</div>
+    <div className == {`responsive-container ${_class}`}>{children}</div>
 );
 
 export default ResponsiveContainer;
